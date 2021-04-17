a7fe326bd70d7aa7d78027cf7926dc152237bb71
Marcus Weiner <marcus.weiner@gmail.com>
2021-04-17 14:04:24 +0200

    Move namespace to main file

--- providers.tf
+++ providers.tf
@@ -17,9 +17,3 @@ provider "kubernetes" {
   host  = "https://${local.kubernetes_host}:8443"
   token = data.terraform_remote_state.kubernetes.outputs.k8s_api_token
 }
-
-resource "kubernetes_namespace" "demo" {
-  metadata {
-    name = "demo"
-  }
-}
